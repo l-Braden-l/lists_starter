1ed3acac85ed153e38c377a3d2df842ad42286d3
braden <leachbr787@northwested.org>
2024-10-21 13:26:08 -0400

    Create python_lists.py

---
+++ python_lists.py
@@ -0,0 +1,31 @@
+# Braden Leach
+# OCT 21 2024
+#Python Lists starter
+
+
+#3-1
+  #list
+names = ['Dakota','Alayna','Ashton','Gideuon']
+  #display
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+
+#3-2
+  #list
+names = ['Dakota','Alayna','Ashton','Gideuon']
+  #display
+print(f'Hello '+ names[0])
+print(f'Hello '+ names[1])
+print(f'Hello '+ names[2])
+print(f'Hello '+ names[3])
+
+#3-3
+  #lists
+transportation = ['Car','Train','Bike','Plane']
+  #display
+print(f'I will hopefully get a ' + transportation[0] +' this year!')
+print(f'I have never rode on a ' + transportation[1] +' before!')
+print(f'I do not know how to ride a ' + transportation[2] + '!')
+print(f'I want to go on a ' + transportation[3] + ' some time in my life!')
